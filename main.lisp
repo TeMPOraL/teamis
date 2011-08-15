4078940962a80331736ba2c9ebb20c45b2539884
Jacek Zlydach <temporal.pl@gmail.com>
2011-08-15 22:32:09 +0100

    Exploring Clack functionality. So far, it works as expected.

--- main.lisp
+++ main.lisp
@@ -2,3 +2,26 @@
 (load (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
 
 (ql:quickload 'clack)
+
+(defun t-index (env)
+  '(200 (:content-type "text/plain") ("INDEX")))
+
+(defun t-random (env)
+  '(200 (:content-type "text/plain") ("RANDOM")))
+
+(defun t-picture (env)
+  '(200 (:content-type "text/plain") ("PICTURE")))
+
+(defun t-newest (env)
+  '(200 (:content-type "text/plain") ("NEWEST")))
+
+(defun t-top (env)
+  '(200 (:content-type "text/plain") ("TOP")))
+
+;;; primary app dispatcher
+(clack.app.route:defroutes t-main (env)
+                           (GET "/" #'t-index)
+                           (GET "/random" #'t-random)
+                           (GET "/:id" #'t-picture)
+                           (GET "/newest" #'t-newest)
+                           (GET "/top" #'t-top))
